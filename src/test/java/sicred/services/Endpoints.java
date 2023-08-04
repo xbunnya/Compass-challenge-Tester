441b9daf5fb8b150ca495e6a13086fa627cf7061
Eduarda Quintana <109098463+xbunnya@users.noreply.github.com>
2023-08-04 13:35:59 -0300

    URL transferida para classe Endpoints e armazenada na String URL

--- src/test/java/sicred/services/Endpoints.java
+++ src/test/java/sicred/services/Endpoints.java
@@ -1,7 +1,8 @@
 package sicred.services;
 
 public class Endpoints {
-    public static final String RESTRICOES = "/restricoes";
-    public static final String SIMULACOES = "/simulacoes";
+    public static final String URL = "http://localhost:8080/api/v1";
+    public static final String RESTRICOES = URL + "/restricoes";
+    public static final String SIMULACOES = URL + "/simulacoes";
 
 }
